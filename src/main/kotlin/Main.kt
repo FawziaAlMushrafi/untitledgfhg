15c8ce8bd914f4adaebd630f90045209eb35d758
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:49:25 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+// total; apples
+const val APPLES =65
+const val DOZENS =12
+
+fun main(args: Array<String>) {
+
+    // write your code her
+    var no0fDozens =APPLES/12
+    var remainapples=APPLES%12
+
+
+
+   println(no0fDozens) //4  of 50
+    println(remainapples) //2 of المتبقي
+
+
+
+}
