6dda0a86f77ecf50053bdb088496c186c870de78
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-20 12:54:28 +0300

    file renane

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,23 @@
-// total; apples
-const val APPLES =65
-const val DOZENS =12
-
-fun main(args: Array<String>) {
-
-    // write your code her
-    var no0fDozens =APPLES/12
-    var remainapples=APPLES%12
-
-
-
-   println(no0fDozens) //4  of 50
-    println(remainapples) //2 of المتبقي
-
-
-
+fun main() {
+    println("Designs a caluator")
+    println("Enter two value")
+    print("a =")
+    var a = readLine()!!.toDouble()
+    print("b =")
+    var b = readLine()!!.toDouble()
+    println("1.Addiion \n2. Substraction \n3. Multiplication \n4. Divide \n5.Modulo ")
+    print("Enter chice:")
+    var f = readLine()!!.toInt()
+    print("result :")
+    when (f) {
+        1 -> print("a+b=${a + b}")
+        2 -> print("a-b=${a - b}")
+        3 -> print("a*b=${a * b}")
+        4 -> print("a/b=${a / b}")
+        5 -> print("a%b=${a % b}")
+        else -> print("Wrong choice")
+    }
 }
+
+
+
